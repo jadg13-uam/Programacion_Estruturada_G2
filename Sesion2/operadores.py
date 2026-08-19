4d21782e21073eae6e5bb9a0daee12ed32f788ca
jadg13 <jose.duran@uamv.edu.ni>
2026-08-19 08:16:14 -0600

    agrego el archivo operadores

---
+++ Sesion2/operadores.py
@@ -0,0 +1,15 @@
+#Leer dos numeros y mostrar la suma, resta, muliplicación, división, modulo
+#Operadores aritmeticos.
+numero1 = int(input("Ingresa un #: "))
+numero2 = int(input("Ingresa otro #: "))
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+divisible = numero1 % numero2 
+
+print(f"Suma: {suma}")
+print(f"Resta: {resta}")
+print(f"Multiplicación: {multiplicacion}")
+print(f"División: {division}")
+print(f"Divisible: {divisible} ")
